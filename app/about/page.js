d1d09c64f8c4497b90218dcad4df5b4ef8475c4d
Zinedine <z_sidane@estin.com>
2024-06-18 17:57:48 +0200

    first commit

---
+++ app/about/page.js
@@ -0,0 +1,7 @@
+export const metadata = {
+  title: "About",
+};
+
+export default function page() {
+  return <h1>about page</h1>;
+}
---
+++ app/cabins/page.js
@@ -0,0 +1,7 @@
+export const metadata = {
+  title: "Cabin",
+};
+
+export default function page() {
+  return <h1>Cabin page</h1>;
+}
